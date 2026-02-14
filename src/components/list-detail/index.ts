257fb680b05770ed57e0fece27aa624096807842
AbdullahAlMaksud <maksud.workspace@gmail.com>
2026-02-14 18:53:17 +0600

    fix: export ListTitleSection from list-detail index

--- src/components/list-detail/index.ts
+++ src/components/list-detail/index.ts
@@ -1,4 +1,5 @@
 export { default as CompletedSection } from './completed-section';
 export { default as ListHeader } from './list-header';
+export { ListTitleSection } from './list-title-section';
 export { default as TotalFooter } from './total-footer';
 
